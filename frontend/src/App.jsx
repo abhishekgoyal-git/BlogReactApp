19df7e0105e96343413250846c6149637a077bf5
abhishekgoyal-git <abhishekkumargoyal197@gmail.com>
2026-06-17 15:00:55 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+    <div>AI BLOGS</div>
+    <ul>
+      <li>Ai in Education </li>
+      <li>Ai in medical  </li>
+      <li>Ai in Technology </li>
+      <li>Ai </li>
+      <li>Ai for </li>
+    </ul>
+    </>
+  )
+}
+
+export default app
