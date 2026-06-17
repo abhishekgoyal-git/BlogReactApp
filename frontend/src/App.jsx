b6b64bc8d7e0915aaf046188839dd77f20a4ec96
abhishekgoyal-git <abhishekkumargoyal197@gmail.com>
2026-06-17 15:21:41 +0530

    added form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,21 @@ export const App = () => {
       <li>Ai in Education </li>
       <li>Ai in medical  </li>
       <li>Ai in Technology </li>
-      <li>Ai </li>
-      <li>Ai for </li>
+      <li>Ai in Agriculture</li>
+      <li>Ai in Transportation </li>
     </ul>
+    <form action="#">
+      <label htmlFor="name">Name</label>
+      <input type="text" id='name' name='name' />
+      <br />
+      <label htmlFor="email">Email</label>
+      <input type="email" id='email' name='email' />
+      <br />
+      <label htmlFor="message">message:</label>
+      <textarea id="message" name="message"></textarea>
+      <br/>
+      <button type="submit">Submit</button>
+    </form>
     </>
   )
 }
